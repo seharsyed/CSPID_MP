236fc0ea9fa42c6d219382bb8e3221d9400dc3a2
seharshah <sehar.naveed@stud-inf.unibz.it>
2018-09-13 16:36:30 +0200

    created directories for source and data files

---
+++ src/cspid.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+//Testing the git commit//
+//This is going to be the main source file and the routines for computations//
+
---
+++ src/util.c
@@ -0,0 +1,5 @@
+//This file is going to contain all routines for managing command line options//
+
+/* ************************
+For debugging and Profiling
+***************************/

ea404babae9633f13ff97358273891e549070536
seharshah <sehar.naveed@stud-inf.unibz.it>
2018-09-14 19:47:29 +0200

    created makefile for cspid, added paths for include and libraries .. dynamic libraries need to be done

--- src/cspid.c
+++ src/cspid.c
@@ -1,5 +1,27 @@
+/*************************************
+  This is going to be the main file
+ *************************************
+ Author:  Sehar Naveed
+ Institute:  Unibz
+ Date: September 14, 2018
+***************************************/
+
+
 #include <stdio.h>
 
-//Testing the git commit//
-//This is going to be the main source file and the routines for computations//
+//Testing the first routine "Hello CSPID"//
 
+int main ()
+{
+
+FILE *fpointer;
+
+fpointer = fopen("output.txt", "w"); 
+
+fprintf(fpointer, "Hello CSPID\n");
+
+fclose(fpointer);
+
+return 0;
+
+}

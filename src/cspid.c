534902eb67c410b6dd708b5f798063c127847074
seharshah <sehar.naveed@stud-inf.unibz.it>
2018-09-17 18:55:44 +0200

    reading input file

--- src/cspid.c
+++ src/cspid.c
@@ -8,21 +8,22 @@
 
 
 #include <stdio.h>
+#include <stdlib.h>
 
 //Testing the first routine "Hello CSPID"//
 
-int main ()
+int main (int arg, char** args)
 {
 
+    //printf("%s :\n\n\n",args[1]);
+//FILE *fpointer;
 
-FILE *fpointer;
-
-fpointer = fopen("output.txt", "w"); 
+//fpointer = fopen("output.txt", "w");
 //This is writing to an output file//
 
-fprintf(fpointer, "Hello CSPID\n");
+//fprintf(fpointer, "Hello CSPID\n");
 
-fclose(fpointer);
+//fclose(fpointer);
 
 
 /*********************************
@@ -36,6 +37,33 @@ fclose(fpointer);
   2) get the size of the matrix
   3) Allocate the memory to the array
   4) Read data */
+    
+    
+    /**
+     File Reading
+     **/
+    
+    FILE * fp;
+    char * line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    if(arg <2){
+        printf("Missing File name\n");
+        return 0;
+    }
+    fp = fopen(args[1], "r");
+    if (fp == NULL)
+        exit(0);
+    
+    while ((read = getline(&line, &len, fp)) != -1) {
+        //printf("Retrieved line of length %zu :\n", read);
+        printf("%s", line);
+    }
+    
+    fclose(fp);
+    if (line)
+        free(line);
+    exit(0);
   
 return 0;
 

9aa6620a39fc47f37f26a95ae2e583127b1d47e2
seharshah <sehar.naveed@stud-inf.unibz.it>
2018-09-17 18:20:18 +0200

    todo list for reading data

--- src/cspid.c
+++ src/cspid.c
@@ -14,14 +14,29 @@
 int main ()
 {
 
+
 FILE *fpointer;
 
 fpointer = fopen("output.txt", "w"); 
+//This is writing to an output file//
 
 fprintf(fpointer, "Hello CSPID\n");
 
 fclose(fpointer);
 
+
+/*********************************
+ * TODO
+*********************************/
+
+/*I need to read data from a file
+ 
+  The format should be 
+  1) open input file
+  2) get the size of the matrix
+  3) Allocate the memory to the array
+  4) Read data */
+  
 return 0;
 
 }
